581879863303364d84f7c7f668f4fd212a2ec01b
tthx <trinhthaihoa@gmail.com>
2019-04-08 03:09:38 +0200

    0.1

--- src/redis_broker/src/main/java/com/orange/redis/service/broker/RedisBindingService.java
+++ src/redis_broker/src/main/java/com/orange/redis/service/broker/RedisBindingService.java
@@ -1,5 +1,9 @@
 package com.orange.redis.service.broker;
 
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.servicebroker.model.binding.CreateServiceInstanceAppBindingResponse;
 import org.springframework.cloud.servicebroker.model.binding.CreateServiceInstanceBindingRequest;
 import org.springframework.cloud.servicebroker.model.binding.CreateServiceInstanceBindingResponse;
@@ -9,57 +13,36 @@ import org.springframework.cloud.servicebroker.model.binding.GetServiceInstanceA
 import org.springframework.cloud.servicebroker.model.binding.GetServiceInstanceBindingRequest;
 import org.springframework.cloud.servicebroker.model.binding.GetServiceInstanceBindingResponse;
 import org.springframework.cloud.servicebroker.service.ServiceInstanceBindingService;
-import org.springframework.stereotype.Service;
 
 @Service
-public
-class RedisBindingService implements ServiceInstanceBindingService {
+public class RedisBindingService implements ServiceInstanceBindingService {
+  private final RedisConfig redisConfig;
 
-  @Override
-  public
-  CreateServiceInstanceBindingResponse createServiceInstanceBinding(CreateServiceInstanceBindingRequest request) {
-    String serviceInstanceId = request.getServiceInstanceId(); String bindingId = request.getBindingId();
-
-    //
-    // create credentials and store for later retrieval
-    //
-
-    String url = new String(/* build a URL to access the service instance */);
-    String bindingUsername = new String(/* create a user */);
-    String bindingPassword = new String(/* create a password */);
-
-    return CreateServiceInstanceAppBindingResponse.builder().credentials("url", url)
-                                                  .credentials("username", bindingUsername)
-                                                  .credentials("password", bindingPassword).bindingExisted(false)
-                                                  .async(true).build();
+  @Autowired
+  public RedisBindingService(RedisConfig redisConfig) {
+    this.redisConfig = redisConfig;
   }
 
   @Override
-  public
-  DeleteServiceInstanceBindingResponse deleteServiceInstanceBinding(DeleteServiceInstanceBindingRequest request) {
-    String serviceInstanceId = request.getServiceInstanceId(); String bindingId = request.getBindingId();
+  public CreateServiceInstanceBindingResponse createServiceInstanceBinding(
+          CreateServiceInstanceBindingRequest request) {
 
-    //
-    // delete any binding-specific credentials
-    //
+    return CreateServiceInstanceAppBindingResponse.builder().async(true)
+                                                  .build();
+  }
 
+  @Override
+  public DeleteServiceInstanceBindingResponse deleteServiceInstanceBinding(
+          DeleteServiceInstanceBindingRequest request) {
     return DeleteServiceInstanceBindingResponse.builder().async(true).build();
   }
 
   @Override
-  public
-  GetServiceInstanceBindingResponse getServiceInstanceBinding(GetServiceInstanceBindingRequest request) {
-    String serviceInstanceId = request.getServiceInstanceId(); String bindingId = request.getBindingId();
-
-    //
-    // retrieve the details of the specified service binding
-    //
-
-    String url = new String(/* retrieved URL */); String bindingUsername = new String(/* retrieved user */);
-    String bindingPassword = new String(/* retrieved password */);
-
-    return GetServiceInstanceAppBindingResponse.builder().credentials("username", bindingUsername)
-                                               .credentials("password", bindingPassword).credentials("url", url)
+  public GetServiceInstanceBindingResponse getServiceInstanceBinding(
+          GetServiceInstanceBindingRequest request) {
+    Map<String, String> credentials;
+    return GetServiceInstanceAppBindingResponse.builder()
+                                               .credentials(credentials)
                                                .build();
   }
 }
---
+++ src/redis_broker/src/main/java/com/orange/redis/service/broker/RedisConfig.java
@@ -0,0 +1,91 @@
+package com.orange.redis.service.broker;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.net.InetAddress;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+@Component
+@ConfigurationProperties(prefix = "orange.redis")
+@Validated
+public class RedisConfig {
+  @NotNull
+  private List<InetAddress> servers = new ArrayList<InetAddress>();
+  @NotNull
+  private int port;
+  @NotNull
+  private String password;
+  @Valid
+  private Sentinel sentinel = new Sentinel();
+
+  public List<InetAddress> getServers() {
+    return servers;
+  }
+
+  public void setServers(List<InetAddress> servers) {
+    this.servers = servers;
+  }
+
+  public int getPort() {
+    return port;
+  }
+
+  public void setPort(int port) {
+    this.port = port;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public Sentinel getSentinel() {
+    return sentinel;
+  }
+
+  public void setSentinel(Sentinel sentinel) {
+    this.sentinel = sentinel;
+  }
+
+  public static class Sentinel {
+    @NotNull
+    private int port;
+    @NotNull
+    private String password;
+    @NotNull
+    private String masterName;
+
+    public int getPort() {
+      return port;
+    }
+
+    public void setPort(int port) {
+      this.port = port;
+    }
+
+    public String getPassword() {
+      return password;
+    }
+
+    public void setPassword(String password) {
+      this.password = password;
+    }
+
+    public String getMasterName() {
+      return masterName;
+    }
+
+    public void setMasterName(String masterName) {
+      this.masterName = masterName;
+    }
+  }
+}
