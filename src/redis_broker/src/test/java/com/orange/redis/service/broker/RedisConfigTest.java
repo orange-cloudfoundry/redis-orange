16f08d409906306333209e05c6b989c9f8e551ca
tthx <trinhthaihoa@gmail.com>
2019-04-09 09:14:13 +0200

    0.1

--- src/redis_broker/src/test/java/com/orange/redis/service/broker/RedisConfigTest.java
+++ src/redis_broker/src/test/java/com/orange/redis/service/broker/RedisConfigTest.java
@@ -1,13 +1,13 @@
 package com.orange.redis.service.broker;
 
+import java.net.InetAddress;
+
 import org.junit.Test;
 import org.junit.Assert;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.net.InetAddress;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
