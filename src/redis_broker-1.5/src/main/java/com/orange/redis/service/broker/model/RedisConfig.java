afdd3c80bfbd9f005bee35b015654273c6f7e041
yellowred-orange <thaihoa.trinh@orange.com>
2020-08-24 23:04:59 +0200

    1.0.3

--- src/redis_broker-1.5/src/main/java/com/orange/redis/service/broker/model/RedisConfig.java
+++ src/redis_broker-1.5/src/main/java/com/orange/redis/service/broker/model/RedisConfig.java
@@ -26,19 +26,37 @@ public class RedisConfig {
   @NotNull
   private String passwordKey;
   @NotNull
+  private String adminUserKey;
+  @NotNull
+  private String adminPasswordKey;
+  @NotNull
   private String HAIPKey;
   @NotNull
   private String HAPortKey;
   @NotNull
   private String HAPasswordKey;
   @NotNull
+  private String tlsPortKey;
+  @NotNull
+  private String tlsClientCERTKey;
+  @NotNull
+  private String tlsClientKeyKey;
+  @NotNull
+  private String tlsCACERTKey;
+  @NotNull
   private List<InetAddress> servers = new ArrayList<>();
   @NotNull
   private Integer port;
   @NotNull
   private String password;
+  @NotNull
+  private String adminUser;
+  @NotNull
+  private String adminPassword;
   @Valid
   private Sentinel sentinel = new Sentinel();
+  @Valid
+  private TLS tls = new TLS();
 
   public String getIPKey() {
     return IPKey;
@@ -62,6 +80,22 @@ public class RedisConfig {
 
   public void setPasswordKey(String passwordKey) {
     this.passwordKey = passwordKey;
+  }
+
+  public String getAdminUserKey() {
+    return adminUserKey;
+  }
+
+  public void setAdminUserKey(String adminUserKey) {
+    this.adminUserKey = adminUserKey;
+  }
+
+  public String getAdminPasswordKey() {
+    return adminPasswordKey;
+  }
+
+  public void setAdminPasswordKey(String adminPasswordKey) {
+    this.adminPasswordKey = adminPasswordKey;
   }
 
   public String getHAIPKey() {
@@ -88,6 +122,38 @@ public class RedisConfig {
     this.HAPasswordKey = HAPasswordKey;
   }
 
+  public String getTLSPortKey() {
+    return tlsPortKey;
+  }
+
+  public void setTLSPortKey(String tlsPortKey) {
+    this.tlsPortKey = tlsPortKey;
+  }
+
+  public String getTLSClientCERTKey() {
+    return tlsClientCERTKey;
+  }
+
+  public void setTLSClientCERTKey(String tlsClientCERTKey) {
+    this.tlsClientCERTKey = tlsClientCERTKey;
+  }
+
+  public String getTLSClientKeyKey() {
+    return tlsClientKeyKey;
+  }
+
+  public void setTLSClientKeyKey(String tlsClientKeyKey) {
+    this.tlsClientKeyKey = tlsClientKeyKey;
+  }
+
+  public String getTLSCACERTKey() {
+    return tlsCACERTKey;
+  }
+
+  public void setTLSCACERTKey(String tlsCACERTKey) {
+    this.tlsCACERTKey = tlsCACERTKey;
+  }
+
   public List<InetAddress> getServers() {
     return servers;
   }
@@ -112,12 +178,36 @@ public class RedisConfig {
     this.password = password;
   }
 
+  public String getAdminUser() {
+    return adminUser;
+  }
+
+  public void setAdminUser(String adminUser) {
+    this.adminUser = adminUser;
+  }
+
+  public String getAdminPassword() {
+    return adminPassword;
+  }
+
+  public void setAdminPassword(String adminPassword) {
+    this.adminPassword = adminPassword;
+  }
+
   public Sentinel getSentinel() {
     return sentinel;
   }
 
   public void setSentinel(Sentinel sentinel) {
     this.sentinel = sentinel;
+  }
+
+  public TLS getTLS() {
+    return tls;
+  }
+
+  public void setTLS(TLS tls) {
+    this.tls = tls;
   }
 
   public static class Sentinel {
@@ -157,6 +247,53 @@ public class RedisConfig {
     }
   }
 
+  public static class TLS {
+
+    private Integer port = null;
+
+    private String clientCERT = null;
+
+    private String clientKey = null;
+
+    private String caCERT = null;
+
+    public Integer getPort() {
+      return port;
+    }
+
+    public void setPort(Integer port) {
+      this.port = port;
+    }
+
+    public String getClientCERT() {
+      return clientCERT;
+    }
+
+    public void setClientCERT(String clientCERT) {
+      this.clientCERT = clientCERT;
+    }
+
+    public String getClientKey() {
+      return clientKey;
+    }
+
+    public void setClientKey(String clientKey) {
+      this.clientKey = clientKey;
+    }
+
+    public String getCaCERT() {
+      return caCERT;
+    }
+
+    public void setCaCERT(String caCERT) {
+      this.caCERT = caCERT;
+    }
+
+    public boolean isEmpty() {
+      return port == null && clientCERT == null && clientKey == null && caCERT == null;
+    }
+  }
+
   public Map<String, Object> toMap() {
     Map<String, Object> credentials = new HashMap<>();
     String servers = new String();
@@ -169,6 +306,10 @@ public class RedisConfig {
     logger.info(getPortKey().concat(" ").concat(getPort().toString()));
     credentials.put(getPasswordKey(), getPassword());
     logger.info(getPasswordKey().concat(" ").concat(getPassword()));
+    credentials.put(getAdminUserKey(), getAdminUser());
+    logger.info(getAdminUserKey().concat(" ").concat(getAdminUser()));
+    credentials.put(getAdminPasswordKey(), getAdminPassword());
+    logger.info(getAdminPasswordKey().concat(" ").concat(getAdminPassword()));
     if (!getSentinel().isEmpty()) {
       credentials.put(getHAIPKey(), getSentinel().getMasterName());
       logger.info(getHAIPKey().concat(" ").concat(getSentinel().getMasterName()));
@@ -176,6 +317,16 @@ public class RedisConfig {
       logger.info(getHAPortKey().concat(" ").concat(getSentinel().getPort().toString()));
       credentials.put(getHAPasswordKey(), getSentinel().getPassword());
       logger.info(getHAPasswordKey().concat(" ").concat(getSentinel().getPassword()));
+    }
+    if (!getTLS().isEmpty()) {
+      credentials.put(getTLSPortKey(), getTLS().getPort().toString());
+      logger.info(getTLSPortKey().concat(" ").concat(getTLS().getPort().toString()));
+      credentials.put(getTLSClientCERTKey(), getTLS().getClientCERT());
+      logger.info(getTLSClientCERTKey().concat(" ").concat(getTLS().getClientCERT()));
+      credentials.put(getTLSClientKeyKey(), getTLS().getClientKey());
+      logger.info(getTLSClientKeyKey().concat(" ").concat(getTLS().getClientKey()));
+      credentials.put(getTLSCACERTKey(), getTLS().getCaCERT());
+      logger.info(getTLSCACERTKey().concat(" ").concat(getTLS().getCaCERT()));
     }
     return credentials;
   }
--- src/redis_broker/src/main/java/com/orange/redis/service/broker/model/RedisConfig.java
+++ src/redis_broker/src/main/java/com/orange/redis/service/broker/model/RedisConfig.java
@@ -27,19 +27,37 @@ public class RedisConfig {
   @NotNull
   private String passwordKey;
   @NotNull
+  private String adminUserKey;
+  @NotNull
+  private String adminPasswordKey;
+  @NotNull
   private String HAIPKey;
   @NotNull
   private String HAPortKey;
   @NotNull
   private String HAPasswordKey;
+  @NotNull
+  private String tlsPortKey;
+  @NotNull
+  private String tlsClientCERTKey;
+  @NotNull
+  private String tlsClientKeyKey;
+  @NotNull
+  private String tlsCACERTKey;
   @NotEmpty
   private List<InetAddress> servers = new ArrayList<>();
   @NotNull
   private Integer port;
   @NotEmpty
   private String password;
+  @NotNull
+  private String adminUser;
+  @NotEmpty
+  private String adminPassword;
   @Valid
   private Sentinel sentinel = new Sentinel();
+  @Valid
+  private TLS tls = new TLS();
 
   public String getIPKey() {
     return IPKey;
@@ -63,6 +81,22 @@ public class RedisConfig {
 
   public void setPasswordKey(String passwordKey) {
     this.passwordKey = passwordKey;
+  }
+
+  public String getAdminUserKey() {
+    return adminUserKey;
+  }
+
+  public void setAdminUserKey(String adminUserKey) {
+    this.adminUserKey = adminUserKey;
+  }
+
+  public String getAdminPasswordKey() {
+    return adminPasswordKey;
+  }
+
+  public void setAdminPasswordKey(String adminPasswordKey) {
+    this.adminPasswordKey = adminPasswordKey;
   }
 
   public String getHAIPKey() {
@@ -89,6 +123,38 @@ public class RedisConfig {
     this.HAPasswordKey = HAPasswordKey;
   }
 
+  public String getTLSPortKey() {
+    return tlsPortKey;
+  }
+
+  public void setTLSPortKey(String tlsPortKey) {
+    this.tlsPortKey = tlsPortKey;
+  }
+
+  public String getTLSClientCERTKey() {
+    return tlsClientCERTKey;
+  }
+
+  public void setTLSClientCERTKey(String tlsClientCERTKey) {
+    this.tlsClientCERTKey = tlsClientCERTKey;
+  }
+
+  public String getTLSClientKeyKey() {
+    return tlsClientKeyKey;
+  }
+
+  public void setTLSClientKeyKey(String tlsClientKeyKey) {
+    this.tlsClientKeyKey = tlsClientKeyKey;
+  }
+
+  public String getTLSCACERTKey() {
+    return tlsCACERTKey;
+  }
+
+  public void setTLSCACERTKey(String tlsCACERTKey) {
+    this.tlsCACERTKey = tlsCACERTKey;
+  }
+
   public List<InetAddress> getServers() {
     return servers;
   }
@@ -113,12 +179,36 @@ public class RedisConfig {
     this.password = password;
   }
 
+  public String getAdminUser() {
+    return adminUser;
+  }
+
+  public void setAdminUser(String adminUser) {
+    this.adminUser = adminUser;
+  }
+
+  public String getAdminPassword() {
+    return adminPassword;
+  }
+
+  public void setAdminPassword(String adminPassword) {
+    this.adminPassword = adminPassword;
+  }
+
   public Sentinel getSentinel() {
     return sentinel;
   }
 
   public void setSentinel(Sentinel sentinel) {
     this.sentinel = sentinel;
+  }
+
+  public TLS getTLS() {
+    return tls;
+  }
+
+  public void setTLS(TLS tls) {
+    this.tls = tls;
   }
 
   public static class Sentinel {
@@ -158,10 +248,56 @@ public class RedisConfig {
     }
   }
 
+  public static class TLS {
+
+    private Integer port = null;
+
+    private String clientCERT = null;
+
+    private String clientKey = null;
+
+    private String caCERT = null;
+
+    public Integer getPort() {
+      return port;
+    }
+
+    public void setPort(Integer port) {
+      this.port = port;
+    }
+
+    public String getClientCERT() {
+      return clientCERT;
+    }
+
+    public void setClientCERT(String clientCERT) {
+      this.clientCERT = clientCERT;
+    }
+
+    public String getClientKey() {
+      return clientKey;
+    }
+
+    public void setClientKey(String clientKey) {
+      this.clientKey = clientKey;
+    }
+
+    public String getCaCERT() {
+      return caCERT;
+    }
+
+    public void setCaCERT(String caCERT) {
+      this.caCERT = caCERT;
+    }
+
+    public boolean isEmpty() {
+      return port == null && clientCERT == null && clientKey == null && caCERT == null;
+    }
+  }
+
   public Map<String, Object> toMap() {
     Map<String, Object> credentials = new HashMap<>();
     String servers = new String();
-    String key;
     for (InetAddress address : getServers())
       servers = servers.concat(address.getHostAddress()).concat(" ");
     servers = servers.trim();
@@ -171,6 +307,10 @@ public class RedisConfig {
     logger.info(getPortKey().concat(" ").concat(getPort().toString()));
     credentials.put(getPasswordKey(), getPassword());
     logger.info(getPasswordKey().concat(" ").concat(getPassword()));
+    credentials.put(getAdminUserKey(), getAdminUser());
+    logger.info(getAdminUserKey().concat(" ").concat(getAdminUser()));
+    credentials.put(getAdminPasswordKey(), getAdminPassword());
+    logger.info(getAdminPasswordKey().concat(" ").concat(getAdminPassword()));
     if (!getSentinel().isEmpty()) {
       credentials.put(getHAIPKey(), getSentinel().getMasterName());
       logger.info(getHAIPKey().concat(" ").concat(getSentinel().getMasterName()));
@@ -178,6 +318,16 @@ public class RedisConfig {
       logger.info(getHAPortKey().concat(" ").concat(getSentinel().getPort().toString()));
       credentials.put(getHAPasswordKey(), getSentinel().getPassword());
       logger.info(getHAPasswordKey().concat(" ").concat(getSentinel().getPassword()));
+    }
+    if (!getTLS().isEmpty()) {
+      credentials.put(getTLSPortKey(), getTLS().getPort().toString());
+      logger.info(getTLSPortKey().concat(" ").concat(getTLS().getPort().toString()));
+      credentials.put(getTLSClientCERTKey(), getTLS().getClientCERT());
+      logger.info(getTLSClientCERTKey().concat(" ").concat(getTLS().getClientCERT()));
+      credentials.put(getTLSClientKeyKey(), getTLS().getClientKey());
+      logger.info(getTLSClientKeyKey().concat(" ").concat(getTLS().getClientKey()));
+      credentials.put(getTLSCACERTKey(), getTLS().getCaCERT());
+      logger.info(getTLSCACERTKey().concat(" ").concat(getTLS().getCaCERT()));
     }
     return credentials;
   }
