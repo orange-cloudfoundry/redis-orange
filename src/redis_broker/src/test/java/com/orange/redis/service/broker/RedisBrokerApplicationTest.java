6b4fa513aa59567f12392b9c2019e057a2b9df48
yellowred-orange <thaihoa.trinh@orange.com>
2021-03-02 09:44:50 +0100

    1.0.3.1

--- src/redis_broker/src/test/java/com/orange/redis/service/broker/RedisBrokerApplicationTest.java
+++ src/redis_broker/src/test/java/com/orange/redis/service/broker/RedisBrokerApplicationTest.java
@@ -1,11 +1,9 @@
 package com.orange.redis.service.broker;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.jupiter.api.Test;
 
-@RunWith(SpringRunner.class)
+import org.springframework.boot.test.context.SpringBootTest;
+
 @SpringBootTest
 public class RedisBrokerApplicationTest {
   @Test
